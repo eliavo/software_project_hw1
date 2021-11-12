e74d891793dc1cce411169df6c67216ec2a08d4a
eliavo <eliavo@mail.tau.ac.il>
2021-11-12 09:11:44 +0200

    now parsing full input file and freeing it

--- kmeans.c
+++ kmeans.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 
 int get_d(FILE*);
+double** parse_input(FILE*);
+void free_data(double**);
 
 int d;
 
 int main(int argc, char* argv[]){
     int K, max_iter;
     FILE *input, *output;
+    double** data;
 
     if (argc<4) {
         perror("Invalid input!\n");
@@ -27,7 +30,12 @@ int main(int argc, char* argv[]){
     }
 
     d = get_d(input);
+    
+    data = parse_input(input);
 
+    printf("%d %d %d %p\n",K, max_iter, d, (void*)data);
+
+    free_data(data);
     fclose(input);
     fclose(output);
     return 0;
@@ -44,4 +52,38 @@ int get_d(FILE *input) {
 
     rewind(input);
     return d;
+}
+
+double** parse_input(FILE *input) {
+    char c, buffer[128];
+    int lines, i, j, offset;
+    double** data;
+
+    for (lines=c=0; (c=fgetc(input))!=EOF; lines += (c=='\n'));
+    
+    data = (double**)malloc(sizeof(double*)*lines);
+    rewind(input);
+
+    for(i=0; i<lines; i++) {
+        data[i] = (double*)malloc(sizeof(double)*d);
+        for (j=0; j<d; j++) {
+            offset = fscanf(input, "%[^,^\n]", buffer);
+            fseek(input, offset, SEEK_CUR);
+            data[i][j] = atof(buffer);
+        }
+    }
+
+    data[lines] = NULL;
+    return data;
+}
+
+void free_data(double** data) {
+    double *entry;
+    int i;
+    
+    i = 0;
+    for (entry=data[i++]; entry!=NULL; entry=data[i++])
+        free(entry);
+    
+    free(data);
 }
